3edb0e0220db85b96f79ce6745bab9ec15d32a5d
José Góes <jose.goes@kroton.com.br>
2020-06-29 02:06:34 -0300

    Fixing print and add import os

--- git.py
+++ git.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 import cx_Oracle as cx
+import os
 
 df = pd.DataFrame(data={'Obj':[1,2,3,4,5,6]})
 
-print('Esse é o resultado: ')
-print(df, end=',')
+print('Esse e o resultado: ')
+print(df, end='')
